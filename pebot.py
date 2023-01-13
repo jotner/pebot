860b25c9e4b07be91921d7313c27866202e340f1
jotner <jonathan.m.eriksson@outlook.com>
2023-01-13 10:41:10 +0100

    Updated savequote command, now stores info and posts to specific channel

--- pebot.py
+++ pebot.py
@@ -28,28 +28,39 @@ async def quote(ctx):
             mylines.append(myline)
 
     response = random.choice(mylines)
-    emb = discord.Embed(title = 'Quote', description = response, color = discord.Colour.nitro_pink())
+    emb = discord.Embed(title = 'Quote', description = f"{response}", color = discord.Colour.nitro_pink())
     await ctx.respond(embed=emb)
 
 # Roll the dice
 @bot.command(name = 'roll', description = 'Roll the dice!')
-async def deathroll(ctx, value=None):
+async def roll(ctx, value=None):
     if value is None :
         value = 100
     new = random.randint(int(1),int(value))
     emb = discord.Embed(title = 'Roll', description = f"{ctx.author.mention} rolls **{new}** (1-{value})")
     await ctx.respond(embed=emb)
 
-# Save a quote to the quotes.txt file
+# Save a quote to TeamPE channel and store it in the quotes.txt file 
 @bot.command(name = 'savequote', description = 'Save a quote')
-async def savequote(ctx, *, quote: str):
+async def savequote(ctx, *, quote: str, name, date=None, time=None):
     file = open('quotes.txt', 'a')
-    file.write(quote)
+    file.write(quote + '\n')
     file.close()
-    emb = discord.Embed(title = 'New quote added!', description = f"Successfully added the quote '*{quote}*'!", color = discord.Colour.green())
+    channel = 360153117938941953
+    if date is None :
+        date = ''
+    if time is None :
+        time = ''
+    emb = discord.Embed(title = 'New quote added!', description = f'Successfully added the quote "*{quote}*"!', color = discord.Colour.green())
     await ctx.respond(embed=emb)
+    await bot.get_channel(int(channel)).send(f'"{quote}" - {name} {date} {time}')
 
-    # channel = bot.get_channel(1062883880618692699)
-    # await channel.send(f"{message.author.mention})
+# Delete the latest quote
+# @bot.command(name = 'deletequote', description = 'Delete the latest quote from the random quote file')
+# async def deletequote(ctx):
+#     file = open('quotes.txt', 'r')
+#     lines = file.readlines()
+#     lines = lines[:-1]
+#     await ctx.respond("Deleted the latest quote.")
 
 bot.run(os.getenv('TOKEN')) # run the bot with the token
